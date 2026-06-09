2b686a54ddbc95199b56c25d2d2fc5c40634e519
Rico Huijbers <rix0rrr@gmail.com>
2026-06-09 14:13:34 +0200

    Make it easier to pick between failure modes

--- lib/stack-problems-stack.ts
+++ lib/stack-problems-stack.ts
@@ -1,32 +1,16 @@
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { ExitOnStartup } from './ecs-exit-on-startup/ecs-exit-on-startup';
 
 export class StackProblemsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'StackProblemsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
-    /*
-    new Bucket(this, 'SomeBucket', {
-      bucketName: 'zomaareenbucket',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-    */
-
-    // new NestedStackWithBucket(this, 'NestyNest');
     // Create an ECS cluster
     const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
-
     const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
 
     // Add capacity to it
@@ -36,42 +20,9 @@ export class StackProblemsStack extends cdk.Stack {
     });
     */
 
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
-    });
-
-    taskDefinition.addContainer('DefaultContainer', {
-      image: ecs.ContainerImage.fromAsset(`${__dirname}/image`),
-      memoryLimitMiB: 512,
-      logging: ecs.LogDrivers.awsLogs({
-        streamPrefix: 'xyz',
-      }),
-    });
-
-    // Instantiate an Amazon ECS Service
-    new ecs.FargateService(this, 'Service', {
-      cluster,
-      taskDefinition,
-      minHealthyPercent: 100,
-      // deploymentStrategy: ecs.DeploymentStrategy.ROLLING,
-      circuitBreaker: {
-        enable: true,
-      },
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-    });
+    //////////////////////////////////////////////////////////////////////
+    // Introduce failures here
+    new ExitOnStartup(this, 'ExitOnStartup', { cluster });
   }
 }
 
-class NestedStackWithBucket extends cdk.NestedStack {
-  constructor(scope: Construct, id: string, props?: cdk.NestedStackProps) {
-    super(scope, id, props);
-
-    new cdk.CfnWaitConditionHandle(this, 'WCH');
-
-    new Bucket(this, 'SomeBucket', {
-      bucketName: 'zomaareenbucket',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-  }
-}
